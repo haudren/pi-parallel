cefcbd6f03f234eba4e5aa5cc5d14e9ecf5912ea
Hervé Audren <h.audren@gmail.com>
2014-06-10 11:20:26 +0900

    New version, now aggregates results from various processes but no real gain

--- src/pi.cxx
+++ src/pi.cxx
@@ -1,18 +1,44 @@
+#include <mpi.h>
 #include <stdio.h>
 #include <iostream>
 #include "pi.hxx"
 
-int main(int argc, const char* argv[])
+int main(int argc, char* argv[])
 {
-	const int steps = 50;
-	double sum = 0;
-	for(int i = 0; i < steps; i++)
-	{
-		sum = sum + term(i);
-	}
+	int masterPID = 0;
+	int tag = 314;
+	int myid, nrprocess;
+	MPI_Status status;
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+	MPI_Comm_size(MPI_COMM_WORLD, &nrprocess);
 	std::cout.precision(15);
-	std::cout << "Pi: " << std::fixed << sum << std::endl;
-	return(1);
+
+	if(myid == 0)
+	{
+		double partial, result;
+		result = 0;
+		for(int pid = 1; pid < nrprocess; pid++)
+		{
+			MPI_Recv(&partial, 1, MPI_DOUBLE, pid, tag,
+					MPI_COMM_WORLD, &status);
+			result = result + partial;
+		}
+		std::cout << "End result : " << result << std::fixed << std::endl;
+	}
+	else
+	{
+		int steps = 50;
+		double sum = 0;
+		for(int i = 0; i < steps; i++)
+		{
+			sum = sum + term(i);
+		}
+		std::cout << "Pi: " << std::fixed << sum << std::endl;
+		MPI_Send(&sum, 1, MPI_DOUBLE, masterPID, tag, MPI_COMM_WORLD);
+	}
+	MPI_Finalize();
 }
 
 double term(int index)
